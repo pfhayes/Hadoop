6704c93bb6882923c6e6122065a483c58a882594
Patrick Hayes <pfhayes@gmail.com>
2014-11-24 17:35:19 -0500

    Add root package

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@
 from setuptools import setup
 
 setup(name='python_hadoop',
-      version='0.1.5',
+      version='0.1.6',
       description='Python Hadoop I/O Utilities',
       license="Apache Software License 2.0 (ASF)",
       author='Matteo Bertozzi',
       author_email='theo.bertozzi@gmail.com',
       url='http://hadoop.apache.org',
-      packages=["python_hadoop.hadoop", 'python_hadoop.hadoop.util', 'python_hadoop.hadoop.io', 'python_hadoop.hadoop.io.compress',
-                "python_hadoop.hadoop.pydoop"],
+      packages=[
+        "python_hadoop",
+        "python_hadoop.hadoop",
+        'python_hadoop.hadoop.util',
+        'python_hadoop.hadoop.io',
+        'python_hadoop.hadoop.io.compress',
+        "python_hadoop.hadoop.pydoop"
+      ],
       extras_require = {
         'pydoop': ['pydoop>=0.9.1']
         }
