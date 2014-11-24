bac5ccf03229328800d4f3f414e9f41151431d73
Patrick Hayes <pfhayes@gmail.com>
2014-11-24 17:38:27 -0500

    missing __init__

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='python_hadoop',
-      version='0.1.6',
+      version='0.1.7',
       description='Python Hadoop I/O Utilities',
       license="Apache Software License 2.0 (ASF)",
       author='Matteo Bertozzi',
